b264cbf9ba9bda38980e96fb974a05e44f1858a0
Parsa-Nasirikhah <108646665+Parsa-Nasirikhah@users.noreply.github.com>
2025-04-02 19:35:42 +0330

    Delete posts directory

--- posts/context_processors.py
+++
@@ -1,3 +0,0 @@
-def categories(request):
-    category = ['Programming', 'food', 'travel']
-    return({'category':category})
--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from posts import views
-
-urlpatterns = [
-    path('', views.Home, name= 'home'),
-    path('<int:id>', views.post, name='post'),
-    path('tags/<int:id>', views.tag, name='tag'),
-    path('search',views.search, name='search')
-]
